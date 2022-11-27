ecd35636f913132ad67e29918a0d64a9a53271fe
Versike <versike727@gmail.com>
2022-11-28 02:26:41 +0300

    create `main` file

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+if __name__ == '__main__':
+    window = Tk()
+    window.title("Queue Visualiztion")
+    window.geometry("1280x1024")
+    window.maxsize(1280,1024)
+    window.minsize(1280,1024)
+    window.config(bg="white")
+    window.mainloop()
