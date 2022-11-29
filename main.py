a29518fb7c317f84cea41fd827477fc733daa72e
Versike <versike727@gmail.com>
2022-11-29 15:58:54 +0300

    edit `main.py`

--- main.py
+++ main.py
@@ -21,28 +21,39 @@ class Queue:
         #Call functions
         self.makeButtons()
         self.kassaParameters()
-    
-    def setKassa(self, kassaNumber, x, y):
-        self.kassaName = Label(self.queue_canvas, text = kassaNumber, fg = "black")
-        self.kassaName.place(x = x, y = y)
         
+    #Buttons settings    
     def makeButtons(self):
         self.startQueues = Button(self.window, text = "Start", fg = "Green", command = self.startQueuesAlgoritm)
         self.startQueues.place(x=30, y=535)
+    
+    #Entry settings        
+    def kassaParameters(self):
+        self.nKassaAmount = Entry(self.window, fg = "black", bg = "white")
+        self.nKassaAmount.place(x=65, y=535)
+    
+    #Function to make kasses
+    def setKassa(self, kassaNumber, x, y):
+        #init name of kassa
+        self.kassaName = Label(self.queue_canvas, text = kassaNumber, fg = "black")
+        self.kassaName.place(x = x, y = y)
         
+        #draw container
+        x2 = x
+        y2 = 200
+        self.queue_canvas.create_line(x, y, x2, y2, fill="yellow", width=4)
+        
+        
+
+    #Main function to start queue
     def startQueuesAlgoritm(self):
         kasses = int(self.nKassaAmount.get())
-        x = 0
+        x = 5
         y = 0
         for kassa in range(kasses):
             self.setKassa(kassa, x, y)
             x += 20
             
-        
-    def kassaParameters(self):
-        self.nKassaAmount = Entry(self.window, fg = "black", bg = "white")
-        self.nKassaAmount.place(x=65, y=535)
-
 if __name__ == '__main__':
     window = Tk()
     window.title("Queue Visualiztion")
