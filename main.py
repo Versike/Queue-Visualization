967c5fd5ad3f2f1615f9ebac5c4b6169fb547633
Versike <versike727@gmail.com>
2022-11-29 18:15:59 +0300

    ебал я графику

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ class Queue:
     def makeButtons(self):
         self.startQueues = Button(self.window, text = "Start", fg = "Green", command = self.startQueuesAlgoritm)
         self.startQueues.place(x=30, y=535)
+        self.pauseQueues = Button(self.window, text = "Stop", fg = "red")
+        self.pauseQueues.place(x=130, y=535)
     
     #Entry settings        
     def kassaParameters(self):
-        self.nKassaAmount = Entry(self.window, fg = "black", bg = "white")
-        self.nKassaAmount.place(x=65, y=535)
+        self.nKassaAmount = Entry(self.window, fg = "black", bg = "white", width=5)
+        self.nKassaAmount.place(x=65, y=540)
     
     #Function to make kasses
     def setKassa(self, kassaNumber, x, y):
@@ -39,20 +41,24 @@ class Queue:
         self.kassaName.place(x = x, y = y)
         
         #draw container
-        x2 = x
         y2 = 200
-        self.queue_canvas.create_line(x, y, x2, y2, fill="yellow", width=4)
-        
+        #left line
+        self.queue_canvas.create_line(x, y, x, y2, fill="blue", width=4)
+        #right line
+        xRightOffset = 30
+        self.queue_canvas.create_line(x+xRightOffset, y, x+xRightOffset, y2, fill="blue", width=4)
+        #upper line
+        self.queue_canvas.create_line(x, y2, x+xRightOffset, y2, fill="red", width=4)
         
 
     #Main function to start queue
     def startQueuesAlgoritm(self):
         kasses = int(self.nKassaAmount.get())
-        x = 5
+        x = 20
         y = 0
         for kassa in range(kasses):
             self.setKassa(kassa, x, y)
-            x += 20
+            x += 100
             
 if __name__ == '__main__':
     window = Tk()
