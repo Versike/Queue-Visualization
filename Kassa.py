32c25931a80135d3cee4a5c3252439bfc22a786d
Versike <versike727@gmail.com>
2022-12-04 13:26:45 +0300

    update

--- Kassa.py
+++ Kassa.py
@@ -1,7 +1,9 @@
 class Kassa():
     def __init__(self):
         self.number = None
-        self.queue = []
+        self.queue = 0
+        self.x = 0
+        self.y = 0
         # self.IsActive = False
         # self.intr = None
         # self.mean = []
